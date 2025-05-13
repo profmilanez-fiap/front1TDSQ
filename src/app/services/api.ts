386870903021dfb02e8f935d0ddbe604363d8b21
Adriano Milanez <pf1588@fiap.com.br>
2025-05-13 11:39:27 -0300

    atualização URL API

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,4 +1,5 @@
 
-const URL_BASE = 'http://localhost:8080/frontmusics/api';
+//const URL_BASE = 'http://localhost:8080/frontmusics/api';
+const URL_BASE = 'https://api1tdsq.onrender.com';
 
 export default URL_BASE;
